085ab3d58413ea521a82922431e9f6609db42380
Yanreni <karl.meng@qq.com>
2022-11-19 18:30:49 +0800

    chore(lualine): 调整底部状态栏样式

--- lua/configs/statusline.lua
+++ lua/configs/statusline.lua
@@ -1,6 +1,16 @@
 local M = {}
 function M.config()
-  require('lualine').setup {}
+  require('lualine').setup {
+    sections = {
+      lualine_c = {
+        {
+          'filename',
+           path = 1,
+        },
+        'filesize'
+      },
+    },
+  }
 end
 
 return M
