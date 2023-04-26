9929603b8e73cc9f8e162e2970fba873a2e546cf
M.k <karl.meng@qq.com>
2023-04-26 16:29:05 +0800

    fix(todo-comments): 🐛 修复图标展示异常问题

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -248,6 +248,20 @@ return require('packer').startup(function(use)
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
+        keywords = {
+          FIX = {
+            icon = " ", -- icon used for the sign, and in search results
+            color = "error", -- can be a hex color, or a named color (see below)
+            alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+            -- signs = false, -- configure signs for some keywords individually
+          },
+          TODO = { icon = " ", color = "info" },
+          HACK = { icon = " ", color = "warning" },
+          WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+          PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+          NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+          TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+        },
       })
     end
   }
@@ -281,8 +295,8 @@ return require('packer').startup(function(use)
 
   -- 格式化
   -- use { 
-  --   'MunifTanjim/prettier.nvim',
-  --   config = function() require('plugins.prettier') end
-  -- }
-end)
+    --   'MunifTanjim/prettier.nvim',
+    --   config = function() require('plugins.prettier') end
+    -- }
+  end)
 
--- lua/plugins/nightly-theme.lua
+++ lua/plugins/nightly-theme.lua
@@ -1,14 +1,14 @@
 require("nightly").setup({
-  color = "blue", -- blue, green or red
-  transparent = false,
+  jzRjlor = "blue", -- blue, green or red
+  transparent = true,
   styles = {
-      comments = { italic = true },
-      functions = { italic= false },
-      keywords = { italic = false },
-      variables = { italic = false },
+    comments = { italic = true },
+    functions = { italic= false },
+    keywords = { italic = false },
+    variables = { italic = false },
   },
   highlights = {
-      -- add or override highlights
-      -- Normal = { bg = "#000000" }
+    -- add or override highlights
+    -- Normal = { bg = "#000000" }
   },
 })
